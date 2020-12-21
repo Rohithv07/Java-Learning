d341dad5d18ec2a57088ab309851fa084c795506
rohithv07 <rohithv63@gmail.com>
2020-12-21 15:34:47 +0530

    Demo for profile

---
+++ 1_LEARNING_PATH_FSE/MavenLearning/profilesdemo/src/main/java/com/rohith/maven/App.java
@@ -0,0 +1,13 @@
+package com.rohith.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
